dbb0207f61e5c3f51cf9417acc99fb0d93138d47
JhessReyes <jhessaureyes@gmail.com>
2022-09-12 17:57:46 -0600

    blade login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,18 +8,47 @@
 </head>
 
 <body>
-    <div class="container mx-auto px-10">
-        <h1 class="text-3xl font-bold underline">
-            How to Install Tailwind CSS in Laravel - ItSolutionStuff.com
-        </h1>
+    <div class="container mx-auto px-10" {{-- style="padding: 20%" --}}>
+        <div class="form-group col-md-12">
+            <div class="row content-center" style="text-align: center">
+                <div class="max-h-full content-center">
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat ncupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button class="btn">Button</button>
+                    <div class="">
+                        <div class="hero min-h-screen min-w-screen bg-base-200" >
+                            <div class="hero-content flex-col lg:flex-row-reverse">
+                                <div class="text-center lg:text-left">
+                                    <h1 class="text-5xl font-bold">Login</h1>
+                                    <p class="py-8">Enter credentials.</p>
+                                </div>
+                                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 glass">
+                                    <div class="card-body">
+                                        <div class="form-control">
+                                            <label class="label">
+                                                <span class="label-text">Email</span>
+                                            </label>
+                                            <input type="text" placeholder="email" class="input input-bordered" />
+                                        </div>
+                                        <div class="form-control">
+                                            <label class="label">
+                                                <span class="label-text">Password</span>
+                                            </label>
+                                            <input type="text" placeholder="password" class="input input-bordered" />
+                                            <label class="label">
+                                                <a href="#" class="label-text-alt link link-hover">Forgot
+                                                    password?</a>
+                                            </label>
+                                        </div>
+                                        <div class="form-control mt-6">
+                                            <button class="btn btn-primary">Login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
