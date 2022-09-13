d14ac6a1371fbae1af8bf6234d5a05d774022e86
JhessReyes <jhessaureyes@gmail.com>
2022-09-12 18:06:03 -0600

    route and resources login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,39 +13,45 @@
             <div class="row content-center" style="text-align: center">
                 <div class="max-h-full content-center">
 
-                    <div class="">
-                        <div class="hero min-h-screen min-w-screen bg-base-200" >
-                            <div class="hero-content flex-col lg:flex-row-reverse">
-                                <div class="text-center lg:text-left">
-                                    <h1 class="text-5xl font-bold">Login</h1>
-                                    <p class="py-8">Enter credentials.</p>
-                                </div>
-                                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 glass">
-                                    <div class="card-body">
-                                        <div class="form-control">
-                                            <label class="label">
-                                                <span class="label-text">Email</span>
-                                            </label>
-                                            <input type="text" placeholder="email" class="input input-bordered" />
-                                        </div>
-                                        <div class="form-control">
-                                            <label class="label">
-                                                <span class="label-text">Password</span>
-                                            </label>
-                                            <input type="text" placeholder="password" class="input input-bordered" />
-                                            <label class="label">
-                                                <a href="#" class="label-text-alt link link-hover">Forgot
-                                                    password?</a>
-                                            </label>
-                                        </div>
-                                        <div class="form-control mt-6">
-                                            <button class="btn btn-primary">Login</button>
+                    <form action="{{ '/login' }}" enctype="multipart/form-data" method="POST">
+                        @method('POST')
+                        @csrf
+                        <div class="">
+                            <div class="hero min-h-screen min-w-screen bg-base-200">
+                                <div class="hero-content flex-col lg:flex-row-reverse">
+                                    <div class="text-center lg:text-left">
+                                        <h1 class="text-5xl font-bold">Login</h1>
+                                        <p class="py-8">Enter credentials.</p>
+                                    </div>
+                                    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                                        <div class="card-body">
+                                            <div class="form-control">
+                                                <label class="label">
+                                                    <span class="label-text">Email</span>
+                                                </label>
+                                                <input type="text" placeholder="email"
+                                                    class="input input-bordered" />
+                                            </div>
+                                            <div class="form-control">
+                                                <label class="label">
+                                                    <span class="label-text">Password</span>
+                                                </label>
+                                                <input type="text" placeholder="password"
+                                                    class="input input-bordered" />
+                                                <label class="label">
+                                                    <a href="#" class="label-text-alt link link-hover">Forgot
+                                                        password?</a>
+                                                </label>
+                                            </div>
+                                            <div class="form-control mt-6">
+                                                <button class="btn btn-primary glass">Login</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
